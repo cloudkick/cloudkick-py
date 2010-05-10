67b7245c7edcf6f2e8b92204c6efed475d4af53f
Paul Querna <pquerna@apache.org>
2010-05-10 15:05:29 -0700

    Fix license files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-# Licensed to the Cloudkick, Inc under one or more
+# Licensed to Cloudkick, Inc ('Cloudkick') under one or more
 # contributor license agreements.  See the NOTICE file distributed with
 # this work for additional information regarding copyright ownership.
-# libcloud.org licenses this file to You under the Apache License, Version 2.0
+# Cloudkick licenses this file to You under the Apache License, Version 2.0
 # (the "License"); you may not use this file except in compliance with
 # the License.  You may obtain a copy of the License at
 #
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from setuptools import setup
 
 setup(
